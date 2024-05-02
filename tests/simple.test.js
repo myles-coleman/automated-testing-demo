2c96ea2f9fbcec02b14db693bb5ea0aeb3ba40c2
Myles Coleman <myles.coleman@liatrio.com>
2024-05-02 13:21:59 -0700

    fixed tests

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -1,36 +1,30 @@
-const test = require('../simple.js').test;
-const addTwo = require('../simple.js').addTwo;
-const multTwo = require('../simple.js').multTwo;
-const subTwo = require('../simple.js').subTwo;
-const divTwo = require('../simple.js').divTwo;
-const expTwo = require('../simple.js').expTwo;
+const { test, addTwo, multTwo, subTwo, divTwo, expTwo } = require('../simple.js');
 
 describe('App', function() {
-    it('test should return 1', function(){
-        assert.equal(test(), 1);
+    it('test should return 1', function() {
+        expect(test()).toBe(1);
     });
-    it('addTwo should return 4', function(){ //addition
-        assert.equal(addTwo(2,2), 4);
+    it('addTwo should return 4', function() { // Addition
+        expect(addTwo(2, 2)).toBe(4);
     });
-    it('multTwo should return 6', function(){ //multiplication
-        assert.equal(multTwo(2,3), 6);
+    it('multTwo should return 6', function() { // Multiplication
+        expect(multTwo(2, 3)).toBe(6);
     });
-    it('subTwo should return 2', function(){ //subtraction
-        assert.equal(subTwo(4,2), 2);
+    it('subTwo should return 2', function() { // Subtraction
+        expect(subTwo(4, 2)).toBe(2);
     });
-    it('divTwo should return 0', function(){ //division by 0
-        assert.equal(divTwo(4,0), 0);
+    it('divTwo should return 0', function() { // Division by 0
+        expect(divTwo(4, 0)).toBe(0);
     });
-    it('divTwo should return 2', function(){ //division
-        assert.equal(divTwo(4,2), 2);
+    it('divTwo should return 2', function() { // Division
+        expect(divTwo(4, 2)).toBe(2);
     });
 
-    //expTwo Unit Tests
-    // it('expTwo should return 4', function(){ //exponentiation
-    //     assert.equal(expTwo(2,2), 4);
+    // expTwo Unit Tests
+    // it('expTwo should return 4', function() { // Exponentiation
+    //     expect(expTwo(2, 2)).toBe(4);
     // });
-    // it('expTwo should return 1', function(){ //exponentiation
-    //     assert.equal(expTwo(2,0), 1);
+    // it('expTwo should return 1', function() { // Exponentiation with power of 0
+    //     expect(expTwo(2, 0)).toBe(1);
     // });
-
 });
