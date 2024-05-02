24a05ec352f0d5eb5f527ade984ba04f466af7f7
Myles Coleman <myles.coleman@liatrio.com>
2024-05-02 13:15:31 -0700

    using jest for testing

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # automated-testing-demo
 
 [![.github/workflows/test.yml](https://github.com/myles-coleman/automated-testing-demo/actions/workflows/test.yml/badge.svg)](https://github.com/myles-coleman/automated-testing-demo/actions/workflows/test.yml)
-
-[![Coverage Status](https://coveralls.io/repos/github/myles-coleman/automated-testing-demo/badge.svg?branch=master)](https://coveralls.io/github/taniarascia/chip8?branch=master)
---
+++ tests/simple.test.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert;
+const test = require('../simple.js').test;
+const addTwo = require('../simple.js').addTwo;
+const multTwo = require('../simple.js').multTwo;
+const subTwo = require('../simple.js').subTwo;
+const divTwo = require('../simple.js').divTwo;
+const expTwo = require('../simple.js').expTwo;
+
+describe('App', function() {
+    it('test should return 1', function(){
+        assert.equal(test(), 1);
+    });
+    it('addTwo should return 4', function(){ //addition
+        assert.equal(addTwo(2,2), 4);
+    });
+    it('multTwo should return 6', function(){ //multiplication
+        assert.equal(multTwo(2,3), 6);
+    });
+    it('subTwo should return 2', function(){ //subtraction
+        assert.equal(subTwo(4,2), 2);
+    });
+    it('divTwo should return 0', function(){ //division by 0
+        assert.equal(divTwo(4,0), 0);
+    });
+    it('divTwo should return 2', function(){ //division
+        assert.equal(divTwo(4,2), 2);
+    });
+
+    //expTwo Unit Tests
+    it('expTwo should return 4', function(){ //exponentiation
+        assert.equal(expTwo(2,2), 4);
+    });
+    it('expTwo should return 1', function(){ //exponentiation
+        assert.equal(expTwo(2,0), 1);
+    });
+
+});
